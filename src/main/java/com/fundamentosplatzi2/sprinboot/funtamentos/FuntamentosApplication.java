99d83ae8ab49f43bb7e5760185f45b0711fd6d55
Oscar Meza <omeza@globalsoft.email>
2023-01-13 10:58:29 -0600

    first commit

---
+++ src/main/java/com/fundamentosplatzi2/sprinboot/funtamentos/FuntamentosApplication.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi2.sprinboot.funtamentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuntamentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuntamentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fundamentosplatzi2/sprinboot/funtamentos/FuntamentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fundamentosplatzi2.sprinboot.funtamentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FuntamentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
